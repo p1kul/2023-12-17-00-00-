9ae4e7c47ef2255c77d276013edb8f0b0296e620
p1kul <funikova@me.com>
2024-08-23 13:23:40 +0300

    Add files via upload

---
+++ ls.py
@@ -0,0 +1,27 @@
+import multiprocessing
+import datetime
+
+def read_info(name):
+    all_data = []
+    with open(name,'r',encoding='utf-8') as file:
+        while file.readline():
+            all_data.append(file.readline())
+
+
+
+filenames = [f'./file {number}.txt' for number in range(1, 5)]
+
+# Линейный вызов
+start1 = datetime.datetime.now()
+for file in filenames:
+    read_info(file)
+fin1 = datetime.datetime.now()
+print(f'{fin1 - start1} - линейный')
+
+# Многопроцессный
+if __name__ == '__main__':
+    with multiprocessing.Pool(processes = 6) as pool:
+        start2 = datetime.datetime.now()
+        pool.map(read_info,filenames)
+    fin2 = datetime.datetime.now()
+    print(f'{fin2 - start2} - многопроцессный')
